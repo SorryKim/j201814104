4453991658f022ce3affd71aad9b715a8afc07d1
Kim, Jaeseong <06zzkimzz06@gmail.com>
2022-10-24 16:55:33 +0900

    Main.java 수정3

--- src/j201814104/Main.java
+++ src/j201814104/Main.java
@@ -1,9 +1,9 @@
 package j201814104;
 
 public class Main {
-    
+
     public static void main(String[] args){
-        String s = "hellow world";
+        String s = "안녕하세요";
         System.out.println(s);
     }
 }
